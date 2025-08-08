12522ba8ba33e8c9193c6d056130e87ed99cac80
Rwano93 <sweez93120@gmail.com>
2025-08-08 03:13:00 +0200

    create: some files + reglages de bug

--- BetAndPlay/BetAndPlay/BetAndPlayApp.swift
+++ BetAndPlay/BetAndPlay/BetAndPlayApp.swift
@@ -2,41 +2,35 @@ import SwiftUI
 
 @main
 struct BetAndPlayApp: App {
-<<<<<<< HEAD
     @StateObject var wallet = WalletService(startingBalance: 1000)
     @StateObject var user = UserService()
     @StateObject var viewRouter = ViewRouter()
-=======
-
-    @StateObject private var wallet = WalletService(startingBalance: 1000)
-    @StateObject private var user = UserService()
->>>>>>> 9d73a983b77b552b03a572e4e6d365f9792d4127
-    
 
     var body: some Scene {
         WindowGroup {
-            switch viewRouter.currentPage {
-            case "splash":
-                SplashView()
-                    .environmentObject(wallet)
-                    .environmentObject(user)
-                    .environmentObject(viewRouter)
-            case "login":
-                LoginView()
-                    .environmentObject(wallet)
-                    .environmentObject(user)
-                    .environmentObject(viewRouter)
-            case "menu":
-                MainMenuView()
-                    .environmentObject(wallet)
-                    .environmentObject(user)
-                    .environmentObject(viewRouter)
-            default:
-                SplashView()
-                    .environmentObject(wallet)
-                    .environmentObject(user)
-                    .environmentObject(viewRouter)
+            RootView()
+                .environmentObject(wallet)
+                .environmentObject(user)
+                .environmentObject(viewRouter)
+                .onAppear { user.restore() }
+        }
+    }
+}
+
+private struct RootView: View {
+    @EnvironmentObject var viewRouter: ViewRouter
+    @EnvironmentObject var user: UserService
+
+    var body: some View {
+        Group {
+            switch user.isLoggedIn ? "menu" : viewRouter.currentPage {
+            case "splash": SplashView()
+            case "login":  LoginView()
+            case "menu":   MainMenuView()
+            default:       SplashView()
             }
         }
+        .animation(.easeInOut, value: viewRouter.currentPage)
+        .transition(.move(edge: .trailing).combined(with: .opacity))
     }
 }
--- BetAndPlay/BetAndPlay/Core/Services/WalletService.swift
+++ BetAndPlay/BetAndPlay/Core/Services/WalletService.swift
@@ -2,16 +2,7 @@ import Foundation
 
 final class WalletService: ObservableObject {
     @Published private(set) var balance: Int
-    
-    init(startingBalance: Int) {
-        self.balance = startingBalance
-    }
-    
-    func add(_ amount: Int) {
-        balance += amount
-    }
-    
-    func remove(_ amount: Int) {
-        balance -= amount
-    }
+    init(startingBalance: Int) { self.balance = startingBalance }
+    func add(_ amount: Int) { balance += amount }
+    func remove(_ amount: Int) { balance = max(0, balance - amount) }
 }
